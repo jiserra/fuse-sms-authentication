aac040c91486051f0abde0414d6031993311486d
Juan Ignacio Serra <jiserra@gmail.com>
2017-02-20 18:11:54 -0500

    Removed all Twilio references for now because of security reasons

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# SMS Authentication example in Fuse
+# SMS Authentication simulation example in Fuse
 
-This is an example of how to authenticate a user using Twilio API to send an SMS and checking the code.
+This is a simulation of how to authenticate a user using a code sent by SMS or other medium.
 
-To make it work you have to:
+To view the correct code, check the Fuse Monitor.
 
-1. Create an account in [Twilio](https://www.twilio.com/)
-2. Buy a Number in Twilio
-3. Change the data in `authentication.js` to match the Number you bought and your Account SID & Token.
-
-### I don't want a Twilio account!
-If you just want to see the UI in action you can change the function of the Go button to `{sendFakeSMS}` instead of `{sendSMS}` and check the Fuse Monitor for the random generated number.
+### TODO
+- Create a server script with security to send a real SMS with Twilio.
+- Tutorial on how to set up server and configure Twilio.
 
 The SMS icon was made by [Alfredo Hernandez](http://www.flaticon.com/authors/alfredo-hernandez "Alfredo Hernandez") from [www.flaticon.com](http://www.flaticon.com "Flaticon") is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/ "Creative Commons BY 3.0")
--- authentication.js
+++ authentication.js
@@ -1,13 +1,8 @@
 var Observable = require("FuseJS/Observable");
-var Base64 = require("FuseJS/Base64");
 
 var phoneNumber = Observable();
 var randomCode = Math.floor(1000 + Math.random() * 9000);
 var phoneMessage = "Your code is " + randomCode;
-
-var fromNumber = "INSERT YOUR FROM NUMBER FROM TWILIO HERE";
-var twilioSID = "TWILIO SID HERE";
-var twilioToken = "TWILIO SECRET TOKEN HERE";
 
 var status = 0;
 var response_ok = false;
@@ -48,32 +43,13 @@ var isCodeValid = codeNumber.map(function(value) {
   }
 });
 
-var authorizationBasic = Base64.encodeAscii(twilioSID + ':' + twilioToken)
-
 function sendSMS() {
-  fetch('https://api.twilio.com/2010-04-01/Accounts/' + twilioSID + '/Messages.json', {
-    method: 'POST',
-    headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8", 'Authorization': 'Basic ' + authorizationBasic, 'Accept': 'application/json'},
-    body: "To=" + phoneNumber.value + "&From=" + fromNumber + "&Body=" + encodeURIComponent(phoneMessage)
-  }).then(function(response) {
-    status = response.status;  // Get the HTTP status code
-    response_ok = response.ok; // Is response.status in the 200-range?
-    return response.json();    // This returns a promise
-  }).then(function(responseObject) {
-    codeSent.value = true;
-  }).catch(function(err) {
-    // An error occurred somewhere in the Promise chain
-  });
-}
-
-function sendFakeSMS() {
   codeSent.value = true;
-  console.log(randomCode);
+  console.log(phoneMessage);
 }
 
 module.exports = {
   sendSMS: sendSMS,
-  sendFakeSMS: sendFakeSMS,
   phoneNumber: phoneNumber,
   codeSent: codeSent,
   codeNumber: codeNumber,
